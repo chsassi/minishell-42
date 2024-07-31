193243ab811b76dd524f52a985dd612cbb2e181a
Bruce-Lutaj <lutajbrus@gmail.com>
2024-07-31 18:17:40 +0200

    Added new function for mtx

--- src/parsing/mtx_handler.c
+++ src/parsing/mtx_handler.c
@@ -6,7 +6,7 @@
 /*   By: brulutaj <brulutaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 16:33:54 by brulutaj          #+#    #+#             */
-/*   Updated: 2024/07/31 15:40:09 by brulutaj         ###   ########.fr       */
+/*   Updated: 2024/07/31 18:14:24 by brulutaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,22 @@
 int	find_number_of_tokens(char *input)
 {
 	int i;
+	int	check;
 	int token;
 
 	i = 0;
 	token = 0;
+	check = 0;
 	while (input && input[i])
 	{
 		if (is_special_char(input[i]))
 			token++;
 		else if (is_quote(input[i]))
-			len_quote_token(input, &i, &token);
+		{
+			check = len_quote_token(input, &i, &token);
+			if (check < 1)
+				return (-1);
+		}
 		else if (is_normal_letter(input, i))
 			len_normal_token(input, &i, &token);
 		i++;
