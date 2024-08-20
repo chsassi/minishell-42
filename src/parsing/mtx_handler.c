5fca1e92b3ba9c34c5746765cb051c4f72c3d8e5
Bruce-Lutaj <lutajbrus@gmail.com>
2024-08-20 10:23:01 +0200

    Added mtx creation

--- src/parsing/mtx_handler.c
+++ src/parsing/mtx_handler.c
@@ -6,7 +6,7 @@
 /*   By: brulutaj <brulutaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 16:33:54 by brulutaj          #+#    #+#             */
-/*   Updated: 2024/08/19 16:36:43 by brulutaj         ###   ########.fr       */
+/*   Updated: 2024/08/20 09:53:14 by brulutaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,17 +57,50 @@ int	token_length(char *input)
 	return(0);
 }
 
+char	**fill_mtx(char **mtx, char *input)
+{
+	int	i;
+	int	len;
+
+	while (*input)
+	{
+		while (*input && *input == ' ')
+			input++;
+		if (*input)
+		{
+			len = token_length(input);
+			mtx[i] = ft_substr(input, 0, len);
+			if (mtx[i] == NULL)
+			{
+				free_mtx(mtx);
+				return (NULL);
+			}
+			i++;
+			input += len;
+		}
+	}
+	mtx[i] == NULL;
+	return (mtx);
+}
+
 char	**create_mtx(char *input)
 {
 	char	**mtx;
 	int		i;
-	int		len;
+	int		number_of_tokens;
 
 	i = 0;
-	len = count_number_of_tokens(input);
-	if (len == -1)
-		return NULL;
-	mtx = (char **)malloc
+	number_of_tokens = count_number_of_tokens(input);
+	if (number_of_tokens == -1)
+		return (ft_putstr_fd("Closing quotes missing", 2), NULL);
+	mtx = (char **)ft_calloc(number_of_tokens, sizeof(char *));
+	if (!mtx || !input)
+	{
+		free(mtx);
+		return (NULL);
+	}
+	
+	
 }
 
 // int	token_len(char *input, enum e_state *state)
