649917a2bc45a7da871558a9d17985078810f948
Bruce-Lutaj <lutajbrus@gmail.com>
2024-09-05 12:20:25 +0200

    New changes

---
+++ src/parsing/expander/expand_var.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_var.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brulutaj <brulutaj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 15:52:46 by brulutaj          #+#    #+#             */
+/*   Updated: 2024/09/04 17:38:18 by brulutaj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+char	*new_expanded_string(char *input, t_env *envp)
+{
+	int	i;
+
+	i = 0;
+	while (input && input[i])
+	{
+		if (input[i] == '$')
+				
+	}
+}
+
+char	*expansion(char *input, t_env *envp)
+{
+	int		n;
+	char	*input_exp;
+	int		i;
+
+	i = 0;
+	n = 0;
+	input_exp = ft_strdup(input);
+	while (input_exp && input_exp[i] != '\0')
+	{
+		count_env_token(input_exp, i, n);
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		input_exp = new_expanded_string(input_exp, envp);
+		i++;
+	}
+	return (input_exp);
+}
