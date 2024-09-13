d71bc3b69bd029d9e099c5f01c2d4d135887bfff
Christian Sassi <christian.sassii@gmail.com>
2024-09-13 18:52:15 +0200

    //Soved SEGFAULT on create_envp()

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -40,46 +40,6 @@ void	bin_cd(t_env *env_list, const char *path)
 	}
 }
 
-void builtin_cd(t_env *env_list, const char *path)
-{
-	t_env *pwd_var = find_env_var(env_list, "PWD");
-	t_env *oldpwd_var = find_env_var(env_list, "OLDPWD");
-
-	if (chdir(path) != 0)
-	{
-		perror("cd");
-		return ;
-	}
-	if (pwd_var)
-	{
-		if (oldpwd_var)
-		{
-			free(oldpwd_var->content);
-			oldpwd_var->content = ft_strdup(pwd_var->content);
-		}
-		else
-		{
-			t_env *new_oldpwd;
-			new_oldpwd = (t_env *)malloc(sizeof(t_env));
-			new_oldpwd->var = ft_strdup("OLDPWD");
-			new_oldpwd->content = ft_strdup(pwd_var->content);
-			new_oldpwd->is_hidden = false;
-			new_oldpwd->next = env_list;
-			env_list = new_oldpwd;
-		}
-	}
-	char	*new_pwd;
-	new_pwd = getcwd(NULL, 0);
-	if (new_pwd)
-	{
-		update_env_var(env_list, "PWD", new_pwd);
-		free(new_pwd);
-	}
-	else
-		perror("cd: getcwd");
-}
-
-
 // da controllare gli argomenti post cd nella lista t_env
 // se il new path e' valido, aggiornare PATH nella lista t_env
 
