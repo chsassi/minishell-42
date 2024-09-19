e0129023911eb0621d4930c6c4f10918507b37eb
Bruce-Lutaj <lutajbrus@gmail.com>
2024-09-19 14:51:07 +0200

    slight changes

--- src/parsing/expander/expand_var.c
+++ src/parsing/expander/expand_var.c
@@ -64,3 +64,50 @@ char	*expansion(char *input, t_env *envp)
 	}
 	return (input_exp);
 }
+
+char	*find_env_string(char *input, t_env *envp, int len)
+{
+	t_env	*tmp;
+	char *substring;
+	
+	tmp = envp;
+	while (tmp)
+	{
+		substring = ft_substr(input, 0, len);
+		if (!ft_strncmp(substring, tmp->var, ft_strlen(substring) + 1))
+		{
+			free(substring);
+			return (ft_strdup(tmp->content));
+		}
+		free(substring);
+		tmp = tmp->next;
+	}
+	return (ft_strdup(""));
+}
+
+char	*env_string(char *input, int *i, t_env *envp)
+{
+	int	j;
+	int	len;
+
+	j = (*i);
+	len = 0;
+    (*i)++;
+    if (input[*i] != '\0')
+    {
+		if (input[*i] == '?')
+		{
+			(*i)++;
+			return (ft_itoa(123));
+		}
+		else if (input[*i] == '_' || ft_isalpha(input[*i]))
+		{
+			(*i)++;
+			while (input[*i] == '_' || ft_isalnum(input[*i]))
+			    (*i)++;
+			len = ((*i) - j) - 1;
+			return (find_env_string(input + (j + 1), envp, len));
+		}
+    }
+	return (ft_strdup("$"));
+}
--- src/parsing/expander/expander_utils.c
+++ src/parsing/expander/expander_utils.c
@@ -12,49 +12,14 @@
 
 #include "parser.h"
 
-char	*find_env_string(char *input, t_env *envp, int len)
+int	is_escaped_char(int c)
 {
-	t_env	*tmp;
-	char *substring;
-	
-	tmp = envp;
-	while (tmp)
-	{
-		substring = ft_substr(input, 0, len);
-		if (!ft_strncmp(substring, tmp->var, ft_strlen(substring) + 1))
-		{
-			free(substring);
-			return (ft_strdup(tmp->content));
-		}
-		free(substring);
-		tmp = tmp->next;
-	}
-	return (ft_strdup(""));
+	if (c == '\'' || c == '\"' || c == '>' || c == '<' || c == '|')
+		return (1);
+	return (0);
 }
 
-char	*env_string(char *input, int *i, t_env *envp)
+char	*processed_str_exp(char *content)
 {
-	int	j;
-	int	len;
-
-	j = (*i);
-	len = 0;
-    (*i)++;
-    if (input[*i] != '\0')
-    {
-		if (input[*i] == '?')
-		{
-			(*i)++;
-			return (ft_itoa(123));
-		}
-		else if (input[*i] == '_' || ft_isalpha(input[*i]))
-		{
-			(*i)++;
-			while (input[*i] == '_' || ft_isalnum(input[*i]))
-			    (*i)++;
-			len = ((*i) - j) - 1;
-			return (find_env_string(input + (j + 1), envp, len));
-		}
-    }
-	return (ft_strdup("$"));
+	char *new
 }
