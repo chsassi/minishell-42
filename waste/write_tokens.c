7c31225463b4dad150fedeaac7f4a25157a63f87
Bruce-Lutaj <lutajbrus@gmail.com>
2024-10-01 14:57:53 +0200

    New stuff

--- waste/write_tokens.c
+++ waste/write_tokens.c
@@ -22,3 +22,23 @@ char *arr_int_str(int i)
         return(ft_strdup("DREDIR_OUT"));
     return(ft_strdup(""));
 }
+
+void	write_mtx2(char **mtx, int *arr)
+{
+	int	i;
+    char *s = NULL;
+
+	i = 0;
+	if (!mtx)
+	{
+		printf("mtx doesn't exist");
+		return ;
+	}
+	while (mtx[i])
+	{
+        s = arr_int_str(arr[i]);
+		printf("[ %s ]__[ %s ]\n", mtx[i], s);
+        free(s);
+		i++;
+	}
+}
