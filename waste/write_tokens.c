77c596427a35e5f58d5d2ba32bc4d5ab48dd8267
Bruce-Lutaj <lutajbrus@gmail.com>
2024-10-01 13:44:40 +0200

    ok

--- waste/write_tokens.c
+++ waste/write_tokens.c
@@ -1,26 +1,24 @@
 #include "minishell.h"
 
-// char *arr_int_str(int i)
-// {
-//     if (i == WORD)
-//         return(ft_strdup("WORD"));
-//     else if(i == QUOTE)
-//         return(ft_strdup("QUOTE"));
-//     else if(i == DOUBLE_QUOTE)
-//         return(ft_strdup("DOUBLE_QUOTE"));
-//     else if(i == ENV)
-//         return(ft_strdup("ENV"));
-//     else if (i == PIPE_LINE)
-//         return(ft_strdup("PIPE_LINE"));
-//     else if (i == REDIR_IN)
-//         return(ft_strdup("REDIR_IN"));
-//     else if (i == REDIR_OUT)
-//         return(ft_strdup("REDIR_OUT"));
-//     else if (i == HERE_DOC)
-//         return(ft_strdup("HERE_DOC"));
-//     else
-//         return(ft_strdup("DREDIR_OUT"));
-//     return(ft_strdup(""));
-// }
-
-// `
+char *arr_int_str(int i)
+{
+    if (i == WORD)
+        return(ft_strdup("WORD"));
+    else if(i == QUOTE)
+        return(ft_strdup("QUOTE"));
+    else if(i == DOUBLE_QUOTE)
+        return(ft_strdup("DOUBLE_QUOTE"));
+    else if(i == ENV)
+        return(ft_strdup("ENV"));
+    else if (i == PIPE_LINE)
+        return(ft_strdup("PIPE_LINE"));
+    else if (i == REDIR_IN)
+        return(ft_strdup("REDIR_IN"));
+    else if (i == REDIR_OUT)
+        return(ft_strdup("REDIR_OUT"));
+    else if (i == HERE_DOC)
+        return(ft_strdup("HERE_DOC"));
+    else
+        return(ft_strdup("DREDIR_OUT"));
+    return(ft_strdup(""));
+}
